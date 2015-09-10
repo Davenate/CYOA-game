10de3f829bf1eb6fc46e807e7a9565ef3eff150a
Davenate <Iamdavenate@yahoo.com>
2015-09-09 21:46:00 -0400

    Added lots of code

--- scripts/gameData.js
+++ scripts/gameData.js
@@ -1,18 +1,18 @@
 var gameData = [
 {
-	name: "path1",
-	test: "Lorem ipsum dolor sit amet, altera repudiare conceptam cum ei. Scripta maluisset ad has. Zril causae omnium vim id. Novum mazim discere no sit, ius in labore laoreet imperdiet. Ad vix mutat nemore inermis, ea tamquam suavitate per. Ex mei epicuri iudicabit vituperata. Per erant repudiare reformidans ne. No ullum imperdiet duo. Mel solum primis equidem ut, propriae lucilius eu quo. Ei eos habeo officiis. Tollit signiferumque ei ius, audiam aperiam oportere ea sea. Ubique luptatum usu et, ex his modo nobis. Est nisl tale similique eu, pri at elitr iuvaret blandit. Ex has omittam pericula, id atqui zril sensibus sit, nec delenit mediocritatem in. Est utinam possit dolores ei. Putent essent id quo, te modus munere honestatis ius. Autem liberavisse eum cu. Ex utinam nostro persius pro. Oblique dissentiet eos ea, ei sed agam dicta nonumes, quo augue alterum bonorum an. Ex magna viris pri. Vel omnesque molestiae ad, est iisque scripta eu. Volutpat theophrastus ei per. Mei et quaerendum delicatissimi. Omnis option cum et, per et sint paulo. Tibique assentior te pri. Ex quem denique eum, id pri wisi lobortis.",
-	charactorsAllowed: [
-		"warror", "mage", "pedestrian"
+	pathName: "Tavern",
+	test: " You're in a tavern, there be a brawl, what will you do?",
+	characters: [
+		"warror", "wizard", "rogue"
 	],
-	pathsFromHere: [ "path2", "path4"]
+	paths: [ "Join in!", "Leave quietly"]
 },
 {
-	name: "path2",
-	test: "Lorem ipsum dolor sit amet, altera repudiare conceptam cum ei. Scripta maluisset ad has. Zril causae omnium vim id. Novum mazim discere no sit, ius in labore laoreet imperdiet. Ad vix mutat nemore inermis, ea tamquam suavitate per. Ex mei epicuri iudicabit vituperata. Per erant repudiare reformidans ne. No ullum imperdiet duo. Mel solum primis equidem ut, propriae lucilius eu quo. Ei eos habeo officiis. Tollit signiferumque ei ius, audiam aperiam oportere ea sea. Ubique luptatum usu et, ex his modo nobis. Est nisl tale similique eu, pri at elitr iuvaret blandit. Ex has omittam pericula, id atqui zril sensibus sit, nec delenit mediocritatem in. Est utinam possit dolores ei. Putent essent id quo, te modus munere honestatis ius. Autem liberavisse eum cu. Ex utinam nostro persius pro. Oblique dissentiet eos ea, ei sed agam dicta nonumes, quo augue alterum bonorum an. Ex magna viris pri. Vel omnesque molestiae ad, est iisque scripta eu. Volutpat theophrastus ei per. Mei et quaerendum delicatissimi. Omnis option cum et, per et sint paulo. Tibique assentior te pri. Ex quem denique eum, id pri wisi lobortis.",
-	charactorsAllowed: [
+	pathName: "path2",
+	test: "Los quem denique eum, id pri wisi lobortis.",
+	characters: [
 		"warror", "pedestrian"
 	],
-	pathsFromHere: [ "path8", "path12", "path34"]
+	paths: [ "path8", "path12", "path34"]
 }
 ]
