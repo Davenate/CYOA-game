e22a4f7abab026bfc70032e56b4840da4b358d26
Davenate <Iamdavenate@yahoo.com>
2015-09-13 00:19:41 -0400

    Code is ready for meeting

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,23 +1,58 @@
-var message = '';
-var game;
-var hero;
+// var message = '';
+// var game;
+// var hero;
 
 
-function print(message) {
-  var outputDiv = document.getElementById('output');
-  outputDiv.innerHTML = message;
-}
+// function print(message) {
+//   var outputDiv = document.getElementById('output');
+//   outputDiv.innerHTML = message;
+// }
 
-function choosePath ( gameData) {
-   var choice = '<h2>PathName: ' + gameData.pathName + '</h2>';
-  choice += '<p>Test ' + gameData.test + '</p>';
-   choice += '<button>Paths: ' + gameData.paths + '</button>';
-  return choice;
-}
+// function choosePath ( gameData) {
+//    var choice = '<h2>PathName: ' + gameData.pathName + '</h2>';
+//   choice += '<p>Test ' + gameData.test + '</p>';
+//    choice += '<button>Paths: ' + gameData.paths + '</button>';
+//   return choice;
+// }
 
-if (gameData.paths === true){
-message = choosePath (gameData);
-	print(message);
-}
+// if (gameData.paths === true){
+// message = choosePath (gameData);
+// 	print(message);
+// }
+
+
+
+
+  var hero = "";
+  function heroChoiceA() {
+    var apprenticeButton = document.getElementById("apprentice");
+    hero = "Apprentice";
+    document.getElementById("ready").innerHTML = "You have chosen the " + (hero)+"."
+    document.getElementById("enter").disabled = false;
+    }
+     function heroChoiceW() {
+    var apprenticeButton = document.getElementById("warrior");
+    hero = "Warrior";
+    document.getElementById("ready").innerHTML = "You have chosen the " + (hero)+"."
+    document.getElementById("enter").disabled = false;
+  }
+   function heroChoiceR() {
+    var apprenticeButton = document.getElementById("rogue");
+    hero = "Rogue";
+    document.getElementById("ready").innerHTML = "You have chosen the " + (hero)+".";
+    document.getElementById("enter").disabled = false;
+  }
+
+    
+
+
+   $(document).ready(function(){
+      $(".enter").click(function(){
+          $("#wrapper").hide();
+          $("#game").show();
+      });
+    });
+
+
 
 
--- scripts/gameData.js
+++ scripts/gameData.js
@@ -1,18 +1,22 @@
 var gameData = [
 {
 	pathName: "Tavern",
-	test: " You're in a tavern, there be a brawl, what will you do?",
+	test: " Your journey begins like most, inside a tavern. You overhear two men from another table taking about a cave deep in the forest that holds a wonderous treasure. Of course, many have entered but none have ever returned. Well, treasure is great but wonderous treasure? That sounds like something to risk your life over! Suddenly on the other side of the tavern you hear someone yell and a see a table get flipped. A brawl errupts in the far end of the tavern. What will you do?",
 	characters: [
-		"warror", "wizard", "rogue"
+		"rogue", "warrior", "apprentice"
 	],
-	paths: [ "Join in!", "Leave quietly"]
+	paths: [ "Join in! I love a good brawl!", "Leave quietly", "try to beak up the fight"],
+	Results:["You got the crap beat out of you and died. Then End", "You quickly exit the tavern and head towards the forest of DOOOOOOM.", "You get hit by a flying barstool and die. The end"]
 },
 {
-	pathName: "path2",
-	test: "Los quem denique eum, id pri wisi lobortis.",
+	pathName: "Road out of town",
+	test: "As you make your way down the road you notice a man leaning casually against a tree. He seems to take particular interest in you and his eyes fix on you as you draw closer. What will you do?",
 	characters: [
-		"warror", "pedestrian"
+		"rogue", "warrior", "apprentice"
 	],
-	paths: [ "path8", "path12", "path34"]
-}
+	paths: [ "Ask him if he has a problem and flex my muscles.", "Quicken my pace and attempt to avoid him.", "Stop and talk to him, maybe hes admiring my dandy shoes."],
+	Results:["You attempt to be intimidating and flex your muscles, or lack therof. Two large men appear from out of the bushes each weilding large clubs. The beat the crap out of you and you die. The End.",	"You begin to walk faster, he takes notice and begins to advance towards you. Just then two large men emerge from the bushes and join with him. You break into a sprint and run as fast as you can. Luckily you were much faster than they were and you got away.",	"You greet the man and ask him if you could be of service. His reply is a snicker as two large men emerge from the bushes behind him. 'Yeah' he chuckles. 'You can give me those shoes' They beat the crap out of you , take your sheos, and you die. Not necessarily in that order. The End"],
+
+
+},
 ]
